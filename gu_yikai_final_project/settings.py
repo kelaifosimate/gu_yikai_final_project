c9843f8e178643852aff7a83b493c30efe1fba01
kelaifosimate <764458769@qq.com>
2025-05-09 20:59:21 -0500

    Complete

--- gu_yikai_final_project/settings.py
+++ gu_yikai_final_project/settings.py
@@ -110,8 +110,9 @@ SESSION_COOKIE_AGE = 60 * 60 * 24 * 7  # One week
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, './static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files configuration
 MEDIA_URL = '/media/'
